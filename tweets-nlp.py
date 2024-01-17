688c8f4f525b6901a7aa22ad2034c3bb50e9ffe2
Kasper Fyhn <kfyhn@cas.au.dk>
2024-01-17 09:52:38 +0100

    More refined NLP script

--- tweets-nlp.py
+++ tweets-nlp.py
@@ -1,11 +1,16 @@
+import argparse
 import glob
 import gzip
+import os.path
+import sys
 from typing import Iterator
 
 import dacy
 import spacy
 import jsonlines
 from tqdm import tqdm
+
+from postingstats.datamodels import MinimalTweet
 
 
 def iter_lines(fp: str) -> Iterator[str]:
@@ -16,24 +21,45 @@ def iter_lines(fp: str) -> Iterator[str]:
                 yield line
 
 
-nlp = spacy.blank("da")  # an empty spacy pipeline
-nlp.add_pipe("dacy/subjectivity")
-nlp.add_pipe("dacy/polarity")
-nlp.add_pipe("dacy/emotionally_laden")  # for emotianal/non-emotional
-nlp.add_pipe("dacy/emotion")  # for type of emotion
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("input_path",
+                        help="Input path of jsonlines file which can also be gzipped"
+                             "and/or split into multiple files. Be mindful of quotes"
+                             "if you use a glob pattern for multiple files.")
+arg_parser.add_argument("--output-path",
+                        default=None,
+                        required=False,
+                        help="Output path of jsonlines output.")
+args = arg_parser.parse_args()
 
-print('Starting')
-reader = jsonlines.Reader(iter_lines('output/test.ndjson/tweets/*'))
-for doc, context in tqdm(nlp.pipe(((obj['text'], obj) for obj in reader), as_tuples=True)):
-    pass
-    # print(doc)
-    # print(doc._.subjectivity)
-    # print(doc._.subjectivity_prob)
-    # print(doc._.polarity)
-    # print(doc._.polarity_prob)
-    # print(doc._.emotionally_laden)
-    # # if emotional print the emotion
-    # if doc._.emotionally_laden == "emotional":
-    #     print("\t", doc._.emotion)
+input_path = args.input_path
+if args.output_path is None:
+    output_path = os.path.join('output', input_path, 'nlp.jsonl')
+    print("Output path will be", output_path)
+else:
+    output_path = args.output_path
+
+print('Starting ...')
+classifications = ["subjectivity", "polarity", "emotionally_laden", "emotion"]
+nlp = spacy.blank("da")  # an empty spacy pipeline
+for classification in classifications:
+    nlp.add_pipe(f"dacy/{classification}")
+
+reader = jsonlines.Reader(iter_lines(input_path), loads=MinimalTweet.from_json)
+doc_pipe = nlp.pipe(((tweet.text, tweet.id) for tweet in reader), as_tuples=True)
+with open(output_path, 'w+') as out:
+    writer = jsonlines.Writer(out)
+    for doc, tweet_id in tqdm(doc_pipe, desc="Processing tweets"):
+        output_dict = {"id": tweet_id}
+        for extension in classifications:
+            output_dict[extension] = getattr(doc._, extension)
+            prob_ext = extension + '_prob'
+            probs = getattr(doc._, prob_ext)
+            output_dict[prob_ext] = {
+                label: float(probs['prob'][i])
+                for i, label in enumerate(probs['labels'])
+            }
+        writer.write(output_dict)
+
 
 
